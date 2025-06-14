44bd5a579536ce67eba3d313b610cb755e7eb81a
Lynndabel <okolo.ebelelynda@gmail.com>
2025-06-14 21:55:29 +0100

    Created a contract folder and moved my contracts/Updated import path

--- src/interfaces/IChapterManager.sol
+++ src/interfaces/IChapterManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../StoryGameTypes.sol";
+import "../contract/StoryGameTypes.sol";
 
 interface IChapterManager {
     function createFirstChapter(
--- src/interfaces/IStoryGameRegistry.sol
+++ src/interfaces/IStoryGameRegistry.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../StoryGameTypes.sol";
+import "../contract/StoryGameTypes.sol";
 
 interface IStoryGameRegistry {
     using StoryGameTypes for *;
